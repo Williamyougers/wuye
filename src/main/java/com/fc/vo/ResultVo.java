372e392ee0f0c36f61d4110e7c1a10f68b47fff5
William_anson <100270948+Williamyougers@users.noreply.github.com>
2022-05-13 18:32:28 +0800

    第er次正式提交 yhx
    1、实现了登录的逻辑（用户登录和管理员登录）
    2、修复了前端页面不显示静态样式（css）和脚本（js）

--- src/main/java/com/fc/vo/ResultVo.java
+++ src/main/java/com/fc/vo/ResultVo.java
@@ -1,13 +1,8 @@
 package com.fc.vo;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ResultVo {
     private Integer code;
     private String message;
