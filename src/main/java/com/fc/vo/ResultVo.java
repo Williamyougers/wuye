3e35fe02311179b9c1bc8ee5b4b0800dc50dbbdd
William_anson <100270948+Williamyougers@users.noreply.github.com>
2022-05-15 13:27:08 +0800

    提交查询

---
+++ src/main/java/com/fc/vo/ResultVo.java
@@ -0,0 +1,11 @@
+package com.fc.vo;
+
+import lombok.Data;
+
+@Data
+public class ResultVo {
+    private Integer code;
+    private String message;
+    private Boolean success;
+    private Object data;
+}
