259bf1557bb09fdd721a047b48277f47a6a2517a
William_anson <100270948+Williamyougers@users.noreply.github.com>
2022-05-15 13:32:10 +0800

    提交查询

---
+++ src/main/java/com/fc/controller/NoticeController.java
@@ -0,0 +1,24 @@
+package com.fc.controller;
+
+import com.fc.entity.Notice;
+import com.fc.service.NoticeService;
+import com.fc.vo.ResultVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("notice")
+public class NoticeController {
+    @Autowired
+    private NoticeService noticeService;
+
+    @PostMapping("add")
+    public ResultVO add(Notice notice){
+    return noticeService.add(notice);
+    }
+}
