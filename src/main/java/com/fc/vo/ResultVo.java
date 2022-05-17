c33a7df73bddf7b5e5e9f65e8e8ce18fd33e79a2
William_anson <100270948+Williamyougers@users.noreply.github.com>
2022-05-17 16:30:49 +0800

    1

--- src/main/java/com/fc/vo/ResultVo.java
+++ src/main/java/com/fc/vo/ResultVo.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ResultVO {
+public class ResultVo {
     private Integer code;
     private String message;
     private Boolean success;
